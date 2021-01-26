fbd6f6eebf2aa10cc11d35613a51cbb84af3dd17
a.gaeini <a.gaeini@AGHIL-PC>
2021-01-26 12:46:17 +0330

    Create UserApi Information

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Utilities;
+
+namespace WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetUserName(long UserID)
+        {
+
+            try
+            {
+                var UserInfo = DataBusiness.FacadeInstaManagerBusiness.GetUserTable().GetByID(UserID);
+                if (UserInfo.IsNull())
+                    throw new Exception("کاربری پیدا نشد");
+
+                return Ok(new
+                {
+                    code = 200,
+                    message = "success",
+                    count = 1,
+                    payload = UserInfo.UserName
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    code = 400,
+                    message = "خطا : " + ex.Message,
+                    count = 1,
+                    payload = 0
+                });
+            }
+
+        }
+
+    }
+}
