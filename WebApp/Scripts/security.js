2366e3eac9ecde7a790f12367a67f10c89436f0a
a.gaeini <a.gaeini@AGHIL-PC>
2021-02-01 21:54:12 +0330

    Create GetAllElements in security.js file

--- WebApp/Scripts/security.js
+++ WebApp/Scripts/security.js
@@ -1,1 +1,23 @@
 ﻿
+function GetAllElemnts() {
+    debugger;
+
+    var all = $('[gid]');
+    var items = {};
+
+    for (i = 0; i < all.length; i++) {
+        if ($(all[i]).attr('gid').length != 0) {
+            var current = {};
+            current.gid = $(all[i]).attr('gid');
+            current.gref = $(all[i]).attr('gref') == "undefined" ? null : $(all[i]).attr('gref');
+            current.title = $(all[i]).attr('title') == "undefined" ? null : $(all[i]).attr('title');
+
+            items[i] = current;
+        }
+    }
+}
+
+GetAllElemnts();
+
+
+
