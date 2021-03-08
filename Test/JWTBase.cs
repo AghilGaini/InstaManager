10f28fa9e28acc2902a202abe2df894ba2df4ea3
a.gaeini <a.gaeini@AGHIL-PC>
2021-03-08 18:19:55 +0330

    Create Test project for JW

---
+++ Test/JWTBase.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    //Example Site : https://medium.com/@mmoshikoo/jwt-authentication-using-c-54e0c71f21b0#:~:text=JWT%20is%20JSON%20Web%20Token,it%20is%20issued%20by%20you.
+
+    public interface IAuthService
+    {
+        string SecretKey { get; set; }
+        bool IsTokenValid(string token);
+        string GenerateToken(IAuthContainerModel model);
+        IEnumerable<Claim> GetTokenClaims(string token);
+    }
+
+    public interface IAuthContainerModel
+    {
+        string SecretKey { get; set; }
+        string SecurityAlgorithm { get; set; }
+        int ExpireMinutes { get; set; }
+        Claim[] claims { get; set; }
+    }
+
+    public class JWTContainerModel : IAuthContainerModel
+    {
+        public string SecretKey { get; set; } = "VGVzdFRlc3Q=";//TestTest
+        public string SecurityAlgorithm { get; set; } = SecurityAlgorithms.HmacSha256Signature;
+        public int ExpireMinutes { get; set; } = 15;
+        public Claim[] claims { get; set; }
+    }
+
+    public class JWTService : IAuthService
+    {
+        public string SecretKey { get; set; }
+
+        public JWTService(string secretKey)
+        {
+            SecretKey = secretKey;
+        }
+
+        private SecurityKey GetSymmetricSecurityKey()
+        {
+            byte[] symmetricKey = Convert.FromBase64String(SecretKey);
+            return new SymmetricSecurityKey(symmetricKey);
+        }
+
+        private TokenValidationParameters GetTokenValidationParameters()
+        {
+            return new TokenValidationParameters()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = GetSymmetricSecurityKey()
+            };
+        }
+
+        public string GenerateToken(IAuthContainerModel model)
+        {
+            if (model == null || model.claims == null || model.claims.Length == 0)
+                throw new Exception("Claims is not valid");
+
+            SecurityTokenDescriptor securityTokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(model.claims),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToInt32(model.ExpireMinutes)),
+                SigningCredentials = new SigningCredentials(GetSymmetricSecurityKey(), model.SecurityAlgorithm)
+            };
+
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+
+            string token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+            return token;
+
+        }
+
+        public IEnumerable<Claim> GetTokenClaims(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("Invalid Token");
+
+            TokenValidationParameters tokenValidationParameters = GetTokenValidationParameters();
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                ClaimsPrincipal tokenValid = jwtSecurityTokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+                return tokenValid.Claims;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public bool IsTokenValid(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                throw new Exception("Token is not valid");
+
+            TokenValidationParameters tokenValidationParameters = GetTokenValidationParameters();
+            JwtSecurityTokenHandler jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+
+            try
+            {
+                ClaimsPrincipal tokenValid = jwtSecurityTokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
